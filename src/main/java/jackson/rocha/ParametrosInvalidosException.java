bb51138148ded300175b247ecaf89ec45e4c3eb4
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-25 20:42:04 -0300

    Criando a classe para tratar as exceções

---
+++ src/main/java/jackson/rocha/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package jackson.rocha;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
